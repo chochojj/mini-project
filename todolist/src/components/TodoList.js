6138dc621dec8f4a0dd3bca6f28c93b6db9d2b18
chochojj <csgy114@gmail.com>
2023-02-02 19:27:07 +0900

    Update TodoList.js

--- todolist/src/components/TodoList.js
+++ todolist/src/components/TodoList.js
@@ -7,6 +7,8 @@ import styled from 'styled-components';
 const TodoList = () => {
   const [todoList, setTodoList] = useState([]);
   const [id, setId] = useState(0);
+  // useEffect는 마운트되고 업데이트 될때 실행되는데 업데이트 될때만 실행시키고 싶을때 쓰는 방법은 useRef(false)해주기
+  // https://xiubindev.tistory.com/100
   const isMount = useRef(true);
 
   useEffect(() => {
